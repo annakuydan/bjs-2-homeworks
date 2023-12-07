13dde695999f57a98079c85fd43db4f42f06bc66
Anna Kuydan <hanni.k@mail.ru>
2023-12-07 23:39:49 +0300

    little changes

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,13 @@
 "use strict"
 function solveEquation(a, b, c) {
   const discriminant = b ** - 4 * a * c;
-  if(discriminant < 0) {
+  if (discriminant < 0) {
     return [];
   } else if (discriminant === 0) {
     const root = -b / (2 * a);
     return [root]; //корень один
   } else {
-    const root1 = (-b + Math.aqrt(discriminant)) / (2 * a);
+    const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
     const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
     return [root1, root2]; //два корня
   }
