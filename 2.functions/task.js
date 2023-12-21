bbc3620aaf8e840306b4210a98f61b51b85ddff1
Anna Kuydan <hanni.k@mail.ru>
2023-12-21 19:10:30 +0300

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,23 @@
 function getArrayParams(...arr) {
+  let min, max, sum, avg;
+  min = infinity;
+  max = -infinity;
 
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+    else if (arr[i] < min) {
+      min = arr[i]
+    }
+    sum = arr[i]
+    avg = (min + max + sum) / 3;
+    avg.toFixed();
+    avg - Number(avg);
+  }
   return { min: min, max: max, avg: avg };
 }
+getArrayParams([1, 5, 8, -100, 10]);
 
 function summElementsWorker(...arr) {
 
