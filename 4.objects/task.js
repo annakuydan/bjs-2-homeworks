6e04313f7ae9d885bcee02c984e7fdbd0f3ac206
hanna <hanni.k@mail.ru>
2024-08-01 20:33:48 +0300

    Implementation of the library

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -33,5 +33,5 @@ Student.prototype.getAverage = function () {
 Student.prototype.exclude = function (reason) {
     delete this.subject;
     delete this.marks;
-    this. excluded = reason;
+    this.excluded = reason;
 }
