40854ebfc2e631596ebb4fc14b5fc8e4a4ede9d8
hanna <hanni.k@mail.ru>
2024-08-01 20:50:27 +0300

    small changes

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,4 +1,4 @@
-задача1
+
 
 class PrintEditionItem {
     constructor(name, releaseDate, pagesCount) {
@@ -28,6 +28,12 @@ class PrintEditionItem {
     }
 }
 
+const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
+
+console.log(sherlock.releaseDate); //2019
+console.log(sherlock.state); //100
+sherlock.fix();
+console.log(sherlock.state); //100
 
 
 class Magazine extends PrintEditionItem {
@@ -66,6 +72,14 @@ class DetectiveBook extends Book {
     }
 }
 
+const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
+
+console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+picknick.state = 10;
+console.log(picknick.state); //10
+picknick.fix();
+console.log(picknick.state); //15
+
 
 //задача 2
 
@@ -96,3 +110,17 @@ class Library {
         }
     }
 }
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
+library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
